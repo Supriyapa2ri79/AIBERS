507a3fc760c1a32c80ddc5dd55eb66ec7ce09590
Supriyapa2ri79 <supriyapa2ri79@gmail.com>
2025-07-16 20:50:35 +0530

    Update RequestRepository.java

--- backend/src/main/java/com/aibers/dashboard/repository/RequestRepository.java
+++ backend/src/main/java/com/aibers/dashboard/repository/RequestRepository.java
@@ -1,1 +1,6 @@
+package com.aibers.dashboard.repository;
 
+import com.aibers.dashboard.model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestRepository extends JpaRepository<Request, Long> {}

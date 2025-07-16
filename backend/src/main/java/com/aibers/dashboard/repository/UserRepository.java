fdae373693256997e2d339dcebd1c39349e85227
Supriyapa2ri79 <supriyapa2ri79@gmail.com>
2025-07-16 20:50:18 +0530

    Update UserRepository.java

--- backend/src/main/java/com/aibers/dashboard/repository/UserRepository.java
+++ backend/src/main/java/com/aibers/dashboard/repository/UserRepository.java
@@ -1,1 +1,8 @@
+package com.aibers.dashboard.repository;
 
+import com.aibers.dashboard.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}

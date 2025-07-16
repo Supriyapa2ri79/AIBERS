34912dd71c6d49399dc635e4f14b5068b4ae95b2
Supriyapa2ri79 <supriyapa2ri79@gmail.com>
2025-07-16 20:27:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
+import Login from "./components/Login";
+import MakerCheckerDashboard from "./components/MakerCheckerDashboard/MakerCheckerDashboard";
+import "./App.css";
 
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {!user ? (
+        <Login onLogin={setUser} />
+      ) : (
+        <MakerCheckerDashboard user={user} />
+      )}
+    </div>
+  );
+}
+
+export default App;

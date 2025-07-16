80abc19ee1934d3e6fc0be758384fc3adf6ac629
Supriyapa2ri79 <supriyapa2ri79@gmail.com>
2025-07-16 20:53:31 +0530

    Update cloudsql_instance.tf

--- gcp/cloudsql_instance.tf
+++ gcp/cloudsql_instance.tf
@@ -1,1 +1,12 @@
-
+resource "google_sql_database_instance" "aibers" {
+  name = "aibers-instance"
+  database_version = "MYSQL_8_0"
+  region = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+resource "google_sql_database" "aibers_db" {
+  name = "aibers_db"
+  instance = google_sql_database_instance.aibers.name
+}
